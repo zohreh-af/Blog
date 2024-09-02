fc63414b03726b3c969dbd378ed638fe40e38b24
zohreh <abbasifar.zohreh@gmail.com>
2024-09-02 15:06:50 +0330

    updating views and templates

--- PersonalBlog/blog/views.py
+++ PersonalBlog/blog/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from PersonalBlog.blog.models import Author, Post
 
 # Create your views here.
 def home_page(request):
-    return render(request,"blog/index.html")
+    post = Post.object.order_by('-updated')[:10]
+    return render(request,"blog/index.html",{"posts":post})
 
 
 def postDetail(request,slug):
-    return render(request,"blog/post-detail.html")
+    post = get_object_or_404(Post,slug)
+    return render(request,"blog/post-detail.html",{"post":post})
 
 def postsList(request):
-    return render(request,"blog/all-posts.html")
+    post = Post.object.order_by('-updated')
+    return render(request,"blog/all-posts.html",{"posts":post})
 
-def authorDetail(request):
-    pass
+def authorDetail(request,id):
+    author = get_object_or_404(Author,id)
+    post = Post.object.filter(author=author).order_by('-updated')[:10]
+    return render(request,"blog/auther-detail.html",{"author":author,"posts":post})
